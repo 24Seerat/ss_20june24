7e9605ab6c926cdf562771cb2fa997f5928d7a72
24Seerat <169182237+24Seerat@users.noreply.github.com>
2024-06-21 12:29:08 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const express=require("express");
+const cors=require("cors");
+const mysql=require("mysql2");
+
+const app=express();
+app.use(cors());
+app.use(express.json());
+
+let con=mysql.createConnection({
+		host:"localhost",
+		user:"root",
+		password:"abc123",
+		database:"ss20june24"
+	});
+app.post("/save",(req,res)=>{
+	let data=[req.body.name,req.body.company,req.body.pkg];
+	let sql="insert into student values(?,?,?)";
+	con.query(sql,data,(err,result)=>{
+		if(err)			res.send(err);
+		else			rews.send(result);
+	});
+});
+app.listen(9000,()=>{console.log("ready to serve @ 9000");});
